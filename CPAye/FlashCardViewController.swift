14cb06eae7a1fcfa8be90847e44f4fdacc517000
hellotojello <tu.alvinj@gmail.com>
2016-10-26 15:33:27 -0400

    Update Flash Card Tap gesture to update its container view

--- CPAye/FlashCardViewController.swift
+++ CPAye/FlashCardViewController.swift
@@ -20,15 +20,155 @@ class FlashCardViewController: UIViewController,UITextViewDelegate {
     let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
     var flashCardInfoVC = UIViewController()
     
+    @IBOutlet weak var flashCardContainerView: UIView!
+    @IBOutlet weak var flashCardTextView: UITextView!
+    @IBOutlet weak var frontTapLabel: UILabel!
+    
+    var back: UITextView!
+    var knowButton : UIButton!
+    var dontKnowButton : UIButton!
+    var singleTap  = UITapGestureRecognizer()
+    
+    var showingAnswer = false
+
+
+    
+    
     override func viewDidLoad() {
         
+        back =  UITextView(frame: CGRect(x: flashCardTextView.frame.origin.x, y: flashCardTextView.frame.origin.y, width: flashCardTextView.frame.size.width, height: flashCardTextView.frame.size.height * 2.5))
+
+        back.delegate = self
+        back.editable = false
+        back.selectable = false
+        back.userInteractionEnabled = false
+        
+        let attrs = [NSFontAttributeName : UIFont.boldSystemFontOfSize(26)]
+        let boldString = NSMutableAttributedString(string:dao.currentFlashCard.concept, attributes:attrs)
+        
+        
+        flashCardTextView.attributedText = boldString
+        flashCardTextView.textAlignment = .Center
+        
+
+       
+
+        
+
+
+
+
+         singleTap = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        singleTap.numberOfTapsRequired = 1
+        
+        flashCardContainerView.addGestureRecognizer(singleTap)
+
+
+        
+//        loadHiddenButtons()
+        
         print("flashCard")
+    }
+    
+    
+    
+    
+    
+    func loadHiddenButtons(){
+        
+//        knowButton.removeFromSuperview()
+//        dontKnowButton.removeFromSuperview()
+//        
+        
+
+        
+        
+        knowButton = UIButton(frame: CGRect(x: 0, y: (back.frame.height - back.frame.height/2), width: back.frame.width/1.5, height: back.frame.height/4))
+        knowButton.backgroundColor = UIColor.greenColor()
+        knowButton.setTitle("I know this word", forState: .Normal)
+        knowButton.addTarget(self, action: #selector(knowThisWord), forControlEvents: .TouchUpInside)
+        
+        self.flashCardContainerView.addSubview(knowButton)
+        
+        dontKnowButton = UIButton(frame: CGRect(x:0, y: (knowButton.frame.origin.y + knowButton.frame.size.height), width: knowButton.frame.width, height: knowButton.frame.height))
+        dontKnowButton.backgroundColor = UIColor.redColor()
+        
+        dontKnowButton.setTitle("I don't know this word", forState: .Normal)
+        dontKnowButton.addTarget(self, action: #selector(dontKnowThisWord), forControlEvents: .TouchUpInside)
+        
+        self.flashCardContainerView.addSubview(dontKnowButton)
+
+
+//        knowButton.hidden = fa
+//        dontKnowButton.hidden = true
+        
+    }
+    func knowThisWord(){
+        
+        print("I know")
+        
+    }
+    
+    func dontKnowThisWord(){
+        
+        print("I don't know.")
+        
     }
     
     
     override func didReceiveMemoryWarning() {
         
     }
+    
+    func tapped() {
+        print("tapped")
+
+        
+        let normalText = "\n\n" + dao.currentFlashCard.definition
+        
+        let boldText  = dao.currentFlashCard.concept
+        
+        let attributedString = NSMutableAttributedString(string:normalText)
+        
+        let attrs = [NSFontAttributeName : UIFont.boldSystemFontOfSize(15)]
+        let boldString = NSMutableAttributedString(string:boldText, attributes:attrs)
+        
+        boldString.appendAttributedString(attributedString)
+        
+        back.attributedText = boldString
+        back.textAlignment = .Left
+
+
+
+//        back.backgroundColor = UIColor.blackColor()
+
+        if(!showingAnswer){
+        UIView.transitionFromView(flashCardTextView, toView: back, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
+            showingAnswer = true
+            
+            flashCardContainerView.removeGestureRecognizer(singleTap)
+
+        
+    }
+        
+        loadHiddenButtons()
+
+    
+        
+
+        
+
+
+
+    }
+    
+    
+    
+    
+    
+    
+    
+
     
     
     
